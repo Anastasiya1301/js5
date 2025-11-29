2af103113e475d065e7999a20d4856522a17cb90
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-11-29 20:00:34 +0200

    p

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ const randomId = uuidv4();
 console.log(randomId);
 
 // uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+// notyf.success();
+      // notyf.error();
+      // notyf.warning();
+    //   document.querySelector(".success").addEventListener("click", () => {
+    //     notyf.success();
+    //   });
+    //   document.querySelector(".error").addEventListener("click", () => {
+    //     notyf.warning();
+    //   });
